e228a7eee71b5a6678cde4649c262f9084fad509
Sahileweb <sahil2003mundhe@gmail.com>
2026-02-11 20:06:11 +0530

    updated sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,17 +2,18 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST, // connects to smtp-relay.brevo.com
-    port: process.env.SMTP_PORT, // connects to 587
-    secure: false, 
-    auth: {
-      user: process.env.SMTP_USER, // uses a2215b001@smtp-brevo.com
-      pass: process.env.SMTP_PASS, // uses your secret key
-    },
-  });
+  host: "smtp-relay.brevo.com",
+  port: 2525, 
+  secure: false, 
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+  connectionTimeout: 10000,
+});
 
   const mailOptions = {
-    from: `SyncEdu <${process.env.EMAIL_FROM}>`, // shows your real email to user
+    from: `SyncEdu <${process.env.EMAIL_FROM}>`, 
     to: options.email,
     subject: options.subject,
     html: options.message,
